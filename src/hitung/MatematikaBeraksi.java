c40eb579053fef8073628363c8eca48379ba470f
hendanugrahpasae <hendanugrahpasae@gmail.com>
2023-06-13 13:51:21 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,14 @@
 package hitung;
 
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika hend = new Matematika(5,4);
+        
+        System.out.println("Hasil penjumlahan: "+hend.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+hend.setPengurangan());
+        System.out.println("Hasil perkalian: "+hend.setPerkalian());
+        System.out.println("Hasil pembagian: "+hend.setPembagian());
+    }
     
 }
